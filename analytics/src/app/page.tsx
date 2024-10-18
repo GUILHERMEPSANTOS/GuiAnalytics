eb174e9ff5e20ef35b29ac9b47399e7a33fe820e
GUILHERMEPSANTOS <guilhermepereiradossantos41@outlook.com>
2024-10-18 14:36:56 -0300

    refactor: correção pt

--- analytics/src/app/page.tsx
+++ analytics/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Wellcome() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">Bem Vindo ao Gui-Analytics</h1>
-      <p className="text-lg text-gray-600 mb-8">Estou muito feliz por vizitar meu website!</p>
+      <p className="text-lg text-gray-600 mb-8">Estou muito feliz por você visitar meu site!</p>
 
       <div className="relative w-48 h-48">
         <Image
